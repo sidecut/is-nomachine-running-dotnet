a6cee95ab359f98f848d39fc3b5460c9e023e93f
James Raden <james.raden@gmail.com>
2022-01-25 04:23:48 -0500

    Create project

---
+++ Data/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace is_nomachine_running_dotnet.Data;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
